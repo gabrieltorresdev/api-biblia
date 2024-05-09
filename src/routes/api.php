719e92bef6257e7fac9743840efe7497b4ed36ec
Gabriel Torres <dev.gabriel.torres@gmail.com>
2024-05-08 21:02:04 -0300

    feat: alterando endpoint de entrada e arquivos docker com o nome do projeto

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/', fn() => response(["app" => "API-GCJUR", "version" => "1.0.0"]));
+    Route::get('/', fn() => response(["app" => "API-BIBLIA", "version" => "1.0.0"]));
 });
 
